2977c814aed4a28d7d1e8046dad3b9fbdb69ed09
AHemenwayKC <38112533+AHemenwayKC@users.noreply.github.com>
2022-11-07 22:38:36 -0600

    Add files via upload
    Updating main java file

--- Test/sos/src/main/java/com/example/App.java
+++ Test/sos/src/main/java/com/example/App.java
@@ -2,40 +2,36 @@
 
 package com.example;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+
 public class App extends Application {
 
-    private static Scene scene;
+
     
     @Override
     public void start(Stage stage) throws IOException {
 
         Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
 
+                
         
-        scene = new Scene(root);
+        Scene scene = new Scene (root);
         stage.setTitle("SOS Game");
         stage.setResizable(false);
 
         stage.setScene(scene);
         stage.show();
-
-
     }
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
