1947e1fc1399c703ecbadb3f62f0970ef56b8941
AHemenwayKC <38112533+AHemenwayKC@users.noreply.github.com>
2022-12-11 19:09:18 -0600

    Add files via upload

--- Test/sos/src/main/java/com/example/Square.java
+++ Test/sos/src/main/java/com/example/Square.java
@@ -1,19 +1,18 @@
 package com.example;
-import java.io.IOException;
 
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.scene.shape.Line;
-import javafx.scene.paint.Color;
 
 
 public class Square extends StackPane {
 
+   private PrimaryController controller;
+
    public Square(int x, int y) {
+      
       setStyle("-fx-border-color: white; -fx-background-color: lightgrey;");
       setId(String.valueOf(x)+"," +String.valueOf(y));
-      this.setPrefSize(2000, 2000);
+      this.setPrefSize(1750, 1750);
       this.setOnMouseClicked(e -> handleMouseClick());
       PrimaryController.boardSpaces.add(this);
    }
@@ -22,113 +21,41 @@ public class Square extends StackPane {
       PrimaryController.makeMove(this);
    }
 
-   public static void drawLine(Square currentSq, Square i, Square j, String direction) {
+   public static void drawLine(Square currSq, Square i, Square j, String direction) {
       switch(direction) {
          case "vertical":
-            Line vertLine1 = new Line((currentSq.getWidth() / 2), 0.0f, (currentSq.getWidth()/2), currentSq.getWidth());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               vertLine1.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               vertLine1.setStroke(Color.BLUE);
-            }
-            currentSq.getChildren().add(vertLine1);
-            Line vertLine2 = new Line((currentSq.getWidth() / 2), 0.0f, (currentSq.getWidth() / 2), currentSq.getWidth());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               vertLine2.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               vertLine2.setStroke(Color.BLUE);
-            }
+            Line vertLine1 = new Line((currSq.getWidth() / 2), 5.0f, (currSq.getWidth() / 2), currSq.getWidth());
+            currSq.getChildren().add(vertLine1);
+            Line vertLine2 = new Line((currSq.getWidth() / 2), 5.0f, (currSq.getWidth() / 2), currSq.getWidth());
             i.getChildren().add(vertLine2);
-            Line vertLine3 = new Line((currentSq.getWidth() / 2), 0.0f, (currentSq.getWidth() / 2), currentSq.getWidth());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               vertLine3.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               vertLine3.setStroke(Color.BLUE);
-            }
+            Line vertLine3 = new Line((currSq.getWidth() / 2), 5.0f, (currSq.getWidth() / 2), currSq.getWidth());
             j.getChildren().add(vertLine3);
             break;
 
          case "horizontal":
-            Line horizLine1 = new Line(0.0f, (currentSq.getHeight() / 2), currentSq.getWidth(), (currentSq.getHeight() / 2));
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               horizLine1.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               horizLine1.setStroke(Color.BLUE);
-            }
-            currentSq.getChildren().add(horizLine1);
-            Line horizLine2 = new Line(0.0f, (currentSq.getHeight() / 2), currentSq.getWidth(), (currentSq.getHeight() / 2));
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               horizLine2.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               horizLine2.setStroke(Color.BLUE);
-            }
+            Line horizLine1 = new Line(5.0f, (currSq.getHeight() / 2), currSq.getWidth(), (currSq.getHeight() / 2));
+            currSq.getChildren().add(horizLine1);
+            Line horizLine2 = new Line(5.0f, (currSq.getHeight() / 2), currSq.getWidth(), (currSq.getHeight() / 2));
             i.getChildren().add(horizLine2);
-            Line horizLine3 = new Line(0.0f, (currentSq.getHeight() / 2), currentSq.getWidth(), (currentSq.getHeight() / 2));
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               horizLine3.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               horizLine3.setStroke(Color.BLUE);
-            }
+            Line horizLine3 = new Line(5.0f, (currSq.getHeight() / 2), currSq.getWidth(), (currSq.getHeight() / 2));
             j.getChildren().add(horizLine3);
             break;
          
          case "diagForward":
-            Line diagForw1 = new Line(currentSq.getWidth(), 0.0f, 0.0f, currentSq.getHeight());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               diagForw1.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               diagForw1.setStroke(Color.BLUE);
-            }
-            currentSq.getChildren().add(diagForw1);
-            Line diagForw2 = new Line(currentSq.getWidth(), 0.0f, 0.0f, currentSq.getHeight());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               diagForw2.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               diagForw2.setStroke(Color.BLUE);
-            }
+            Line diagForw1 = new Line(currSq.getWidth(), 5.0f, 5.0f, currSq.getHeight());
+            currSq.getChildren().add(diagForw1);
+            Line diagForw2 = new Line(currSq.getWidth(), 5.0f, 5.0f, currSq.getHeight());
             i.getChildren().add(diagForw2);
-            Line diagForw3 = new Line(currentSq.getWidth(), 0.0f, 0.0f, currentSq.getHeight());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               diagForw3.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               diagForw3.setStroke(Color.BLUE);
-            }
-            j.getChildren().add(diagForw1);
+            Line diagForw3 = new Line(currSq.getWidth(), 5.0f, 5.0f, currSq.getHeight());
+            j.getChildren().add(diagForw3);
             break;
 
          case "diagBack":
-            Line diagBack1 = new Line(10.0f, 10.0f, (currentSq.getWidth() - 20), (currentSq.getHeight() - 20));
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               diagBack1.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               diagBack1.setStroke(Color.BLUE);
-            }
-            currentSq.getChildren().add(diagBack1);
-            Line diagBack2 = new Line(0.0f, 0.0f, currentSq.getWidth(), currentSq.getHeight());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               diagBack2.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               diagBack2.setStroke(Color.BLUE);
-            }
+            Line diagBack1 = new Line(5.0f, 5.0f, (currSq.getWidth()), (currSq.getHeight() - 20));
+            currSq.getChildren().add(diagBack1);
+            Line diagBack2 = new Line(5.0f, 5.0f, currSq.getWidth(), currSq.getHeight());
             i.getChildren().add(diagBack2);
-            Line diagBack3 = new Line(0.0f, 0.0f, currentSq.getWidth(), currentSq.getHeight());
-            if (PrimaryController.getPlayerTurn().equals("R")) {
-               diagBack3.setStroke(Color.RED);
-            }
-            else if (PrimaryController.getPlayerTurn().equals("B")) {
-               diagBack3.setStroke(Color.BLUE);
-            }
+            Line diagBack3 = new Line(5.0f, 5.0f, currSq.getWidth(), currSq.getHeight());
             j.getChildren().add(diagBack3);
             break;
    
